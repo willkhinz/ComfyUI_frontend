bd1e40b325d9d9e77887b7950eed99341dc856e3
Austin Mroz <austin@comfy.org>
2026-03-30 11:24:02 -0700

    Rework minsize code to better support tests

--- src/services/litegraphService.ts
+++ src/services/litegraphService.ts
@@ -76,7 +76,7 @@ import { useExtensionService } from './extensionService'
 import { useMaskEditor } from '@/composables/maskeditor/useMaskEditor'
 
 export interface HasInitialMinSize {
-  _initialMinSize: { width: number; height: number }
+  _initialMinSize?: { width: number; height: number }
 }
 
 export const CONFIG = Symbol()
@@ -163,7 +163,7 @@ export const useLitegraphService = () => {
    * @internal The key for the node definition in the i18n file.
    */
   function nodeKey(node: LGraphNode): string {
-    return `nodeDefs.${normalizeI18nKey(node.constructor.nodeData!.name)}`
+    return `nodeDefs.${normalizeI18nKey(node.constructor.nodeData?.name ?? '')}`
   }
   /**
    * @internal Add input sockets to the node. (No widget)
@@ -270,6 +270,7 @@ export const useLitegraphService = () => {
       })
     }
     const castedNode = node as LGraphNode & HasInitialMinSize
+    castedNode._initialMinSize ??= { width: 1, height: 1 }
     castedNode._initialMinSize.width = Math.max(
       castedNode._initialMinSize.width,
       minWidth
@@ -327,8 +328,9 @@ export const useLitegraphService = () => {
       node.widgets?.length &&
       !useSettingStore().get('LiteGraph.Node.DefaultPadding')
     const castedNode = node as LGraphNode & HasInitialMinSize
-    s[0] = Math.max(castedNode._initialMinSize.width, s[0] + (pad ? 60 : 0))
-    s[1] = Math.max(castedNode._initialMinSize.height, s[1])
+    const minSize = castedNode._initialMinSize ?? { width: 1, height: 1 }
+    s[0] = Math.max(minSize.width, s[0] + (pad ? 60 : 0))
+    s[1] = Math.max(minSize.height, s[1])
     node.setSize(s)
   }
 
